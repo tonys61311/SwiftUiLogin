0031b6ea35188d94c006ad0f6277e03a53d8abad
Hsing-Chen <hsing-chen.lin@i-mps.com>
2022-01-23 13:26:48 +0800

    Initial Commit

---
+++ SwiftUiLogin/SwiftUiLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiLoginApp.swift
+//  SwiftUiLogin
+//
+//  Created by 林星辰 on 2022/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
